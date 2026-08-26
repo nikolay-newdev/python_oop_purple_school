26065653e70fb74c3e812a3ce5f3cd3f1547a270
nikolay <nik.fss@gmail.com>
2026-08-26 11:19:05 +0300

    Вторая попытка. Функция может принимать неограниченное количество аргументов

--- 2-decorators-dz/2-decorators.py
+++ 2-decorators-dz/2-decorators.py
@@ -1,43 +1,36 @@
 def limit_args(max_value, mode):
     def decorator(func):
         def wrapper(*args, **kwargs):
-            a, b = args
-            if a <= max_value and b <= max_value:
-                result = func(a, b)
-                print(result)
-                return result
-            if a > max_value:
-                if mode == "error":
-                    raise ValueError('E')
+            numbers = []
+            for arg in args:
+                if arg > max_value and mode == "error":
+                    raise ValueError('Error!')
+                elif arg > max_value:
+                    numbers.append(max_value)
                 else:
-                    a = max_value
-            if b > max_value:
-                if mode == "error":
-                    raise ValueError('E')
-                else:
-                    b = max_value
-            result = func(a, b)
+                    numbers.append(arg)
+            result = func(*numbers)
             print(result)
-            return result
-
-            # x1 = a <= max_value
-            # x2 = b <= max_value
-            # if x1 and x2: 
+            return(result)               
+        
+            # a, b = args
+            # if a <= max_value and b <= max_value:
             #     result = func(a, b)
             #     print(result)
             #     return result
-            # elif not(x1 and x2) and mode == "error":
-            #     raise ValueError('Error!')
-            # else: 
-            #     if (a <= max_value and b > max_value or a > max_value and b <= max_value) and mode == "clip":
-            #         mn, mx = min(a, b), max(a, b)
-            #         result = func(mn, max_value)
-            #         print(result)
-            #         return result
+            # if a > max_value:
+            #     if mode == "error":
+            #         raise ValueError('E')
             #     else:
-            #         result = func(max_value, max_value)
-            #         print(result)
-            #         return result
+            #         a = max_value
+            # if b > max_value:
+            #     if mode == "error":
+            #         raise ValueError('E')
+            #     else:
+            #         b = max_value
+            # result = func(a, b)
+            # print(result)
+            # return result
 
         return wrapper
     return decorator
